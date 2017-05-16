5097e7018ea4c52cf7521c9f3798f91a7d78e6c2
enrro <edgarbg585@gmail.com>
2017-05-16 10:12:06 -0500

    tag_uri_and_name is a pretty descriptive method that allows to separate the ure and the name of the element. I use this method to iterate and find the data soruce reference. Child.text returns the string that is inside the datasource carats.

--- source/ReadFilesRDL.py
+++ source/ReadFilesRDL.py
@@ -1,11 +1,18 @@
 import xml.etree.ElementTree as ET
 
+def tag_uri_and_name(elem):
+    if elem.tag[0] == "{":
+        uri, ignore, tag = elem.tag[1:].partition("}")
+    else:
+        uri = None
+        tag = elem.tag
+    return uri, tag
+
 
 tree = ET.parse('../resources/CMF Analyst Metrics Report.xml')
 root = tree.getroot()
-
 print(root.tag)
-print(root.attrib)
+uri = tag_uri_and_name(root)
 
 for child in root:
     print(child.tag)
@@ -16,9 +23,12 @@ print("\n next \n")
 print(root[1][0][0])
 print(root[1][0][0].text)
 
+print("\n The DataSourceReference is \n")
 
-#for child in root.iter():
-#print(child.tag)
-#print(child.attrib)
-#if child.tag == "{http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition}DataSources":
-#print("reasd")
+
+for child in root.iter('{' + uri[0] + '}' + "DataSourceReference"):
+    #print(child.tag)
+    #print(child.attrib)
+    print(child.text)
+    # if child.tag == "{http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition}DataSources":
+    #     print("reasd")
