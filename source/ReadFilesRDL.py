967045fb7e7113a8676abf121f53edd057965fdb
Enrro <edgarbg585@gmail.com>
2017-05-15 19:08:41 -0500

    encontre que DataSourceName se encuentra en [1][0][0] esto podria no ser igual para todos los archivos y hay que revisarlo

--- source/ReadFilesRDL.py
+++ source/ReadFilesRDL.py
@@ -3,10 +3,21 @@ import xml.etree.ElementTree as ET
 
 tree = ET.parse('../resources/CMF Analyst Metrics Report.xml')
 root = tree.getroot()
-print(root.tag, root.attrib)
-#for child in root.iter():
-    #print(child.tag)
-    #print(child.attrib)
-    #if child.tag == "{http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition}DataSources":
-        #print("reasd")
 
+print(root.tag)
+print(root.attrib)
+
+for child in root:
+    print(child.tag)
+
+print("\n next \n")
+
+print(root[1][0][0])
+print(root[1][0][0].text)
+
+
+#for child in root.iter():
+#print(child.tag)
+#print(child.attrib)
+#if child.tag == "{http://schemas.microsoft.com/sqlserver/reporting/2010/01/reportdefinition}DataSources":
+#print("reasd")
