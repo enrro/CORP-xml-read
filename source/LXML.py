cead645c5c3fec0dffd9981cd2668f5a30f84d43
enrro <edgarbg585@gmail.com>
2017-05-17 11:57:59 -0500

    now we write to a csv file

--- source/LXML.py
+++ source/LXML.py
@@ -1,7 +1,7 @@
 from lxml import etree
 import csv
 
-someiterable = ["hi"]
+someiterable = ["hi", '\n', "hola"]
 someiterable1 = [1,2,3,4,5]
 import csv
 writer = csv.writer(open("some.csv", "w"), dialect='excel', lineterminator='\n')
--- source/ReadFilesRDL.py
+++ source/ReadFilesRDL.py
@@ -11,45 +11,45 @@ def tag_uri_and_name(elem):
         tag = elem.tag
     return uri, tag
 
-
+writer = csv.writer(open("some.csv", "w"), dialect='excel', lineterminator='\n')
 filesList = (os.listdir("../resources/"))
+header =["name of file","number of stored procedures","number of procedure","name of stored procedures","number of queue"]
+writer.writerow(header)
+information = ["","","","",""]
 
 for file in filesList:
 
 
     tree = ET.parse('../resources/' + file)
     root = tree.getroot()
-    #print(root.tag)
     uri = tag_uri_and_name(root)
 
-
-
-
     print(file + " DataSourceReference is")
-
+    information[0] = file
 
     #iterate through the array to find the DataSoruce and find the attributes by name
     for child in root.iter('{' + uri[0] + '}' + "DataSource"):
-        print(child.attrib['Name'])
-
+        information[1] =(child.attrib['Name'])
+    #iterate for procedures
     numberOfStoredProcedures = 0
     for child in root.iter('{' + uri[0] + '}' + "CommandType"):
         #print(child.text)
         numberOfStoredProcedures = numberOfStoredProcedures + 1
     # for child1 in root.iter('{' + uri[0] + '}' + "CommandText"):
     print("number of stop procedures = ", numberOfStoredProcedures)
-
+    information[2] = numberOfStoredProcedures
+    #iterate for queries
     numberOfQueries = 0
+    nameOfStoredProcedures = ""
     for child in root.iter('{' + uri[0] + '}' + "CommandText"):
         if numberOfStoredProcedures > 0:
-            print(child.text)
+            nameOfStoredProcedures = nameOfStoredProcedures + " " + (child.text)
             numberOfStoredProcedures = numberOfStoredProcedures - 1
         else:
             numberOfQueries = numberOfQueries + 1
 
-
-
-
+    information[3] = nameOfStoredProcedures
     print("number of queues ", numberOfQueries)
-
+    information[4] = numberOfQueries
     print('\n')
+    writer.writerow(information)
