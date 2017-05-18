fff423f4900f44e54982f258b01f510009c369a1
enrro <edgarbg585@gmail.com>
2017-05-18 11:04:22 -0500

    line 17 to 20 help to find all of the folders that are inside the folder download, I refactored this so now we iterate throught all of the folders inside downloads

--- source/ReadFilesRDL.py
+++ source/ReadFilesRDL.py
@@ -11,45 +11,59 @@ def tag_uri_and_name(elem):
         tag = elem.tag
     return uri, tag
 
-writer = csv.writer(open("some.csv", "w"), dialect='excel', lineterminator='\n')
-filesList = (os.listdir("../resources/"))
-header =["name of file","number of stored procedures","number of procedure","name of stored procedures","number of queue"]
+
+goalDirectory = "D:/Users/gdlebric/Downloads/"
+
+folders = []
+for f in os.listdir(goalDirectory):
+    if not f == "desktop.ini":
+        folders.append(f)
+
+
+
+writer = csv.writer(open("report.csv", "w"), dialect='excel', lineterminator='\n')
+header =["directory","name of file","Data Source","number of procedure","name of stored procedures","number of Queries"]
 writer.writerow(header)
-information = ["","","","",""]
+for directory in folders:
+    filesList = (os.listdir(goalDirectory + "/" + directory))
+    information = ["","","","","",""]
 
-for file in filesList:
+    for file in filesList:
 
+        information[0] = directory
+        tree = ET.parse(goalDirectory + directory + "/" + file)
+        root = tree.getroot()
+        uri = tag_uri_and_name(root)
 
-    tree = ET.parse('../resources/' + file)
-    root = tree.getroot()
-    uri = tag_uri_and_name(root)
+        print(file + " DataSourceReference is")
+        information[1] = file
 
-    print(file + " DataSourceReference is")
-    information[0] = file
+        #iterate through the array to find the DataSoruce and find the attributes by name
+        dataSourceName = ""
+        for child in root.iter('{' + uri[0] + '}' + "DataSource"):
+            dataSourceName = dataSourceName + " " + child.attrib['Name']
+        information[2] = dataSourceName
+        #iterate for procedures
+        numberOfStoredProcedures = 0
+        for child in root.iter('{' + uri[0] + '}' + "CommandType"):
+            #print(child.text)
+            numberOfStoredProcedures = numberOfStoredProcedures + 1
 
-    #iterate through the array to find the DataSoruce and find the attributes by name
-    for child in root.iter('{' + uri[0] + '}' + "DataSource"):
-        information[1] =(child.attrib['Name'])
-    #iterate for procedures
-    numberOfStoredProcedures = 0
-    for child in root.iter('{' + uri[0] + '}' + "CommandType"):
-        #print(child.text)
-        numberOfStoredProcedures = numberOfStoredProcedures + 1
-    # for child1 in root.iter('{' + uri[0] + '}' + "CommandText"):
-    print("number of stop procedures = ", numberOfStoredProcedures)
-    information[2] = numberOfStoredProcedures
-    #iterate for queries
-    numberOfQueries = 0
-    nameOfStoredProcedures = ""
-    for child in root.iter('{' + uri[0] + '}' + "CommandText"):
-        if numberOfStoredProcedures > 0:
-            nameOfStoredProcedures = nameOfStoredProcedures + " " + (child.text)
-            numberOfStoredProcedures = numberOfStoredProcedures - 1
-        else:
-            numberOfQueries = numberOfQueries + 1
+        # for child1 in root.iter('{' + uri[0] + '}' + "CommandText"):
+        print("number of stop procedures = ", numberOfStoredProcedures)
+        information[3] = numberOfStoredProcedures
+        #iterate for queries
+        numberOfQueries = 0
+        nameOfStoredProcedures = ""
+        for child in root.iter('{' + uri[0] + '}' + "CommandText"):
+            if numberOfStoredProcedures > 0:
+                nameOfStoredProcedures = nameOfStoredProcedures + " " + (child.text)
+                numberOfStoredProcedures = numberOfStoredProcedures - 1
+            else:
+                numberOfQueries = numberOfQueries + 1
 
-    information[3] = nameOfStoredProcedures
-    print("number of queues ", numberOfQueries)
-    information[4] = numberOfQueries
-    print('\n')
-    writer.writerow(information)
+        information[4] = nameOfStoredProcedures
+        print("number of queues ", numberOfQueries)
+        information[5] = numberOfQueries
+        print('\n')
+        writer.writerow(information)
