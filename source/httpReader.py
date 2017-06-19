5c4726f8457664505355759cd9c5f998f0084d30
enrro <edgarbg585@gmail.com>
2017-06-19 12:31:07 -0500

    new data web scrapper

---
+++ source/httpReader.py
@@ -0,0 +1,24 @@
+import os
+from os.path import join, getsize
+import http.client
+import requests
+from requests.auth import HTTPBasicAuth
+from requests_ntlm import HttpNtlmAuth
+username = 'AMERICAS\\gdlebric'
+password = 'Qeehs46'
+url = 'http://sacnte335/Reports/Pages/Folder.aspx'
+
+s = requests.Session()
+s.auth = HttpNtlmAuth(username, password)
+
+r1 = s.get(url)
+
+print ('r1', r1)
+
+r1 = requests.get(url, auth=HTTPBasicAuth(username, password))
+
+print ('r1', r1)
+print(r1.headers)
+'''r = requests.get("http://sacnte335/Reports/Pages/Folder.aspx",auth=('domain/gdlebric','Qeehs46,'))
+print(r.status_code)
+'''
